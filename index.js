d0ab07c35bcf74751680dc3454849550d86b1065
Kevin Nielsen <knielsen24@gmail.com>
2022-04-27 14:43:24 -0700

    commit tree c7896b363ddc0ab85565362543ba45e53201a016

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+
+//var upperCaseCustomerName = () => customerName = customerName.toUpperCase();
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();
+   return customerName;
+}
+// upperCaseCustomerName();
+
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+//console.log(setBestCustomer());
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+
+//console.log(setBestCustomer());
+
+const leastFavoriteCustomer = "ted";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "bob";
+    return leastFavoriteCustomer;
+}
